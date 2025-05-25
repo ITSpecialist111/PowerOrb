9db0905f50d0ba2db2005b27731ac2a8819ad77d
Graham Hosking <142685548+ITSpecialist111@users.noreply.github.com>
2025-05-25 09:45:06 +0100

    updpates

--- power_orb/run.sh
+++ power_orb/run.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/with-contenv sh
-set -e
-
-# Load add-on options
-CONFIG_PATH="/data/options.json"
-ENTITY_ID=$(jq --raw-output ".entity_id" "$CONFIG_PATH")
-
-# Build React frontend
-cd /data/frontend
-npm ci
-npm run build
-
-# Serve build
-cd build
-exec python3 -m http.server 80
+# Serve the pre-compiled React bundle over port 80 for Home Assistant ingress.
+exec python3 -m http.server 80 -d /opt/power_orb/www
